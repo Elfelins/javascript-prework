1893f41dee2f70c12b0c846e4a66542f3e80aaca
Elfelins <50467996+Elfelins@users.noreply.github.com>
2023-09-22 23:46:08 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,9 @@
-var computerMove;
-var playerMove;
-computerMove = 'kamień';
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
